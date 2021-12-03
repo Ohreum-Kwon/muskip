d2ace8867de0ab59c9e32dcd5c915138df146d93
iceicebaebee <ohreumkwon@gmail.com>
2021-12-02 20:40:08 -0800

    first commit

---
+++ src/Body.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Body.css";
+import Header from "./Header";
+import { useStateValue } from "./StateProvider";
+import SongRow from "./SongRow";
+
+function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useStateValue();
+
+  const playPlaylist = (id) => {
+    spotify
+      .play({
+        context_uri: `spotify:playlist:37i9dQZF1EfQuqi3vRoAZ3`,
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+  const playSong = (id) => {
+    spotify
+      .play({
+        uris: [`spotify:track:${id}`],
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+  return (
+    <div className="body">
+      <Header spotify={spotify} />
+
+      <div className="body__songs">
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow playSong={playSong} track={item.track} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import ProfileIcon from "@material-ui/icons/Face";
+import { Avatar } from "@material-ui/core";
+import { getTokenFromResponse } from "./spotify";
+import { useStateValue } from "./StateProvider";
+
+function Sidebar() {
+  const [{ playlists, user }, dispatch] = useStateValue();
+  console.log(playlists);
+
+  return (
+    <div className="sidebar">
+        <div className="sidebar__top">
+        <Avatar alt={user?.display_name} src={user?.images[0].url} />
+        <h4>{user?.display_name}</h4>
+        </div>
+      <SidebarOption Icon={HomeIcon} option="Home" />
+      <SidebarOption Icon={ProfileIcon} option="Profile" />
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption option={playlist.name} />
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
