447a67fe168bfdc4ecc922e0c7d4ab7441bda89f
hzkSJSU <78756164+hzkSJSU@users.noreply.github.com>
2021-12-12 21:07:16 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// This file will  takes a React element root of an element tree and a container DOM element as arguments
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
